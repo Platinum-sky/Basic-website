4915324f56a62f320c110033427ac9fa5001f76d
Eric Monestime <61268054+Platinum-sky@users.noreply.github.com>
2025-01-21 15:36:00 -0500

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Eric | thank you</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/contact-style.css" />
+	<link href="css/eric_style.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		
+			
+		<br />
+		Thanks you, and have a nice day. <br />
+		
+			
+		<h1></h1><br />
+		
+		<p><a href="index.html">Retrn to Home</a></p>
+	
+	</div>
+	
+	
+</body>
+
+</html>
